d4cf48de7a9eaa1c11938bf90df9a3096f762885
Adrian Rachman <adrian.y.rachman@gmail.com>
2025-04-15 17:31:01 +0800

    First Commit

--- Reporthink_Template_Editor_Project/Template_Editor_App/urls.py
+++ Reporthink_Template_Editor_Project/Template_Editor_App/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("", views.index, name="index"),
   path('choose-template/', views.choose_template, name='choose_template'),
   path('edit-template/', views.edit_template, name='edit_template'),
+  path('template-to-be-edited/', views.template_to_be_edited, name='template_to_be_edited'),
 ]
--- Reporthink_Template_Editor_Project/Template_Editor_App/views.py
+++ Reporthink_Template_Editor_Project/Template_Editor_App/views.py
@@ -10,3 +10,5 @@ def choose_template(request):
      return render(request, 'choose-template.html')
 def edit_template(request):
      return render(request, 'edit-template.html')
+def template_to_be_edited(request):
+     return render(request, 'template-to-be-edited.html')
